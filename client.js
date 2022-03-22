76b9e0b60a9c2ed98200a187c8b5259bc0e8cc45
Michael Fitzgerald <michael.fitzgerald@freshconsulting.com>
2022-03-22 13:05:57 -0700

    WIP

---
+++ client.js
@@ -0,0 +1,8 @@
+import React from "https://dev.jspm.io/react@17.0.2"
+import ReactDOM from "https://dev.jspm.io/react-dom@17.0.2"
+import App from "./app.js"
+
+ReactDOM.hydrate(
+  <App />,
+  document.getElementById("root"),
+)
